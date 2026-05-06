42aa85cfd17f723e4ee4712cba0e882db447c8c8
Dhruv Vaviya <dhruvvaviyablog@gmail.com>
2026-05-06 09:40:12 +0530

    related-doctor,about-us,contact-us page added

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -33,7 +33,7 @@ const Doctors = () => {
                                 ? navigate("/doctors")
                                 : navigate("/doctors/General physician")
                         }
-                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}
                     >
                         General physician
                     </p>
@@ -43,7 +43,7 @@ const Doctors = () => {
                                 ? navigate("/doctors")
                                 : navigate("/doctors/Gynecologist")
                         }
-                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}
                     >
                         Gynecologist
                     </p>
@@ -53,7 +53,7 @@ const Doctors = () => {
                                 ? navigate("/doctors")
                                 : navigate("/doctors/Dermatologist")
                         }
-                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}
                     >
                         Dermatologist
                     </p>
@@ -63,7 +63,7 @@ const Doctors = () => {
                                 ? navigate("/doctors")
                                 : navigate("/doctors/Pediatricians")
                         }
-                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Pediatricians" ? "bg-indigo-100 text-black" : ""}`}
                     >
                         Pediatricians
                     </p>
@@ -73,7 +73,7 @@ const Doctors = () => {
                                 ? navigate("/doctors")
                                 : navigate("/doctors/Neurologist")
                         }
-                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Neurologist" ? "bg-indigo-100 text-black" : ""}`}
                     >
                         Neurologist
                     </p>
@@ -83,7 +83,7 @@ const Doctors = () => {
                                 ? navigate("/doctors")
                                 : navigate("/doctors/Gastroenterologist")
                         }
-                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gastroenterologist" ? "bg-indigo-100 text-black" : ""}`}
                     >
                         Gastroenterologist
                     </p>
