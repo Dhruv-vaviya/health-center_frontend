edb26bd218a9b234ac7b98888ffeb6628bb4072c
Dhruv Vaviya <dhruvvaviyablog@gmail.com>
2026-05-07 10:00:30 +0530

    mobile-menu added

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -5,6 +5,7 @@ import { AppContext } from "../context/AppContext";
 const Doctors = () => {
     const { speciality } = useParams();
     const [filterDoc, setFilterDoc] = useState([]);
+    const [showFilter, setShowFilter] = useState(false);
 
     const { doctors } = useContext(AppContext);
 
@@ -26,7 +27,8 @@ const Doctors = () => {
         <div>
             <p className="text-gray-600">Browse through the doctors specialist.</p>
             <div className="flex flex-col sm:flex-row  items-start gap-5 mt-5">
-                <div className="flex flex-col gap-4 text-sm text-gray-600">
+                <button className={`py-1px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-[#5F6FFF] text-white' : ''}`} onClick={() => setShowFilter(prev => !prev)}>Filters</button>
+                <div className={`"flex flex-col gap-4 text-sm text-gray-600" ${showFilter ? 'flex' : 'hidden'} sm:flex`}>
                     <p
                         onClick={() =>
                             speciality === "General physician"
